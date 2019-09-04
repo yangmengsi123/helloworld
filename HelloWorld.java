9a6ef53f8dafe8ce8355ce663053876b44b1062d
Yang <Yangmengsi@126.com>
2019-09-04 19:58:35 +0800

    created a helloworld class

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+//HelloWorld.java
+/*@author:Miss.Yang
+  @data:2019/9/4 下午18:00
+*/
+public class HelloWorld{
+   public static void main(String[]args{
+       System.out.println("Hello World");
+	   }
+  }
+  
