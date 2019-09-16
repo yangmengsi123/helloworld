81ff1439d3f917a2423a6e7ee1c5c375fef6ddb2
zhuo <zhuo@123.com>
2019-09-16 21:24:50 +0800

    Class2 class

---
+++ Class2.java
@@ -0,0 +1,10 @@
+public class Class2 {
+	puclic static void main(String[] args) {
+		int count = 10;
+		int cout = 0;
+		count *= count + 2;
+		count /= 100;
+		cout++;
+		System.out.printf("%d",count);
+	}
+}
