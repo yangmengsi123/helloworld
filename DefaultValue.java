c4ff516be1726a0ba9aaf73bbadee4700b1d900e
zhuo <zhuo@123.com>
2019-10-28 15:19:56 +0800

    DefaultValue class

---
+++ DefaultValue.java
@@ -0,0 +1,10 @@
+package com.classbook.chapter.six;
+
+public class DefaultValue {
+    //图书的编码必须包含“INB”
+    //设置一个常量，名为BOOK_SKU
+    final public static String DICTIONARY_SKU = "D001";
+    final public static String content(){
+        return "INB";
+    }
+}
